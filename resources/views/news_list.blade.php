f7091d1c9c00f33f32b80ef47fd0c0a900958392
Amsocat <vasilyaka.dr@dvfu.ru>
2022-01-20 22:44:41 +1000

    Revert "Lesson 10: Add web and API auth and profile"
    This reverts commit 2384de15

--- resources/views/news_list.blade.php
+++ resources/views/news_list.blade.php
@@ -23,19 +23,6 @@
     @include('layouts.app')
 </head>
 <body class="antialiased">
-@if (Route::has('login'))
-    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-        @auth
-            <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
-        @else
-            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-
-            @if (Route::has('register'))
-                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-            @endif
-        @endauth
-    </div>
-@endif
 <h1>Новости</h1>
 @foreach($news as $i => $news_item)
     <p>
